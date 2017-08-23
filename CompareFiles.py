9224d32abd16e1c113a0fdc0884fffff1807b34d
Anders Dahl Henriksen <anders.dahl.henriksen@philips.com>
2017-08-23 14:09:38 +0200

    -Implemented prints console -Implemented calls to program logic in CompareFiles.py

--- CompareFiles.py
+++ CompareFiles.py
@@ -2,52 +2,69 @@ import numpy as np
 import pandas as pd
 
 # Hard coded path, should be input from gui later
-DataFolder = r'/media/anders/-Anders-3-/Google Drev/Novozymes/'
-#DataFolder = r'C:\Users\310229518\Google Drive\Novozymes\'
-database_path = DataFolder + 'Access Novozymes.xlsx'
-vilkar_path = DataFolder + '2017 AUG LABTEK TOTAL.xlsx'
-hk_path = DataFolder + 'Ba 2017 juni NovoZymes LAB.xlsx'
-maillist_path = DataFolder + 'outlook aug 2017.txt'
+# DataFolder = r'/media/anders/-Anders-3-/Google Drev/Novozymes/'
+# #DataFolder = r'C:\Users\310229518\Google Drive\Novozymes\'
+# database_path = DataFolder + 'Access Novozymes.xlsx'
+# vilkar_path = DataFolder + '2017 AUG LABTEK TOTAL.xlsx'
+# hk_path = DataFolder + 'Ba 2017 juni NovoZymes LAB.xlsx'
+# maillist_path = DataFolder + 'outlook aug 2017.txt'
 
-# Read database excel file
-database = pd.read_excel(database_path).dropna(axis=1, how='all')
-medlem_bool = database['Medlem'].str.lower() == 'ja'
 
-# Read labktek excel file
-vilkar = pd.read_excel(vilkar_path, skiprows=4).dropna(axis=1, how='all')
+def read_db(database_path):
+    """ Read database excel file """
+    database = pd.read_excel(database_path).dropna(axis=1, how='all')
+    medlem_bool = database['Medlem'].str.lower() == 'ja'
+    return database, medlem_bool
 
-# Read hk excel file
-hk = pd.read_excel(hk_path).dropna(axis=1, how='all')
 
-# Read mailing list txt file
-maillist_list = []
-with open(maillist_path, 'rb') as f:
-    maillist_list.append([word for line in f for word in str(line).split(';')])
+def read_vilkar(vilkar_path):
+    """ Read labktek excel file """
+    vilkar = pd.read_excel(vilkar_path, skiprows=4).dropna(axis=1, how='all')
+    return vilkar
 
-maillist_df = pd.DataFrame(np.squeeze(maillist_list))
-maillist = maillist_df[0].str.extract(r'<(\w+@\w+.\w+)>', expand=False)
 
-# Compare to vilkar
-init_db = set(database['Initialer'].dropna().str.upper())
-init_vilkar = set(vilkar['Initials'].dropna().str.upper())
-init_onlydb = init_db.difference(init_vilkar)
-init_onlyvilkar = init_vilkar.difference(init_db)
-print('The following initials are only in NovoZymes database:\n' + '\n'.join(init_onlydb))
-print('The following initials are only in Vilkaar database:\n' + '\n'.join(init_onlyvilkar))
+def read_hk(hk_path):
+    """ Read hk excel file """
+    hk = pd.read_excel(hk_path).dropna(axis=1, how='all')
+    return hk
 
-# Compare to hk
-cpr_db = set(database['CPR-nummer'].dropna().str.zfill(10).str.replace('-', ''))  # [medlem_bool] might be needed
-cpr_hk = set(hk['CPR'].dropna().str.replace('-', ''))
-cpr_onlyhk = cpr_hk.difference(cpr_db)
-cpr_onlydb = cpr_db.difference(cpr_hk)
-print('The following cpr are only in NovoZymes database:\n' + '\n'.join(cpr_onlydb))
-print('The following cpr are only in HK database:\n' + '\n'.join(cpr_onlyhk))
 
-# Compare mailing list to database based on email
-mails_db = set(database['Mailadresse'][medlem_bool].dropna().str.lower())
-mails_txt = set(maillist.dropna().str.lower())
-mails_del = mails_txt.difference(mails_db)
-mails_add = mails_db.difference(mails_txt)
-print('The following mails can be deleted from outlook:\n' + '\n'.join(mails_del))
-print('The following mails can be added to outlook:\n' + '\n'.join(mails_add))
+def read_maillist(maillist_path):
+    """ Read mailing list txt file """
+    maillist_list = []
+    with open(maillist_path, 'rb') as f:
+        maillist_list.append([word for line in f for word in str(line).split(';')])
 
+    maillist_df = pd.DataFrame(np.squeeze(maillist_list))
+    maillist = maillist_df[0].str.extract(r'<(\w+@\w+.\w+)>', expand=False)
+    return maillist
+
+
+def compare_vilkar(database, vilkar):
+    """ Compare main database to vilkar database """
+    init_db = set(database['Initialer'].dropna().str.upper())
+    init_vilkar = set(vilkar['Initials'].dropna().str.upper())
+    init_onlydb = init_db.difference(init_vilkar)
+    init_onlyvilkar = init_vilkar.difference(init_db)
+    print('The following initials are only in NovoZymes database:\n' + '\n'.join(init_onlydb))
+    print('The following initials are only in Vilkaar database:\n' + '\n'.join(init_onlyvilkar))
+
+
+def compare_hk(database, hk):
+    """ Compare main database to HK database"""
+    cpr_db = set(database['CPR-nummer'].dropna().str.zfill(10).str.replace('-', ''))  # [medlem_bool] might be needed
+    cpr_hk = set(hk['CPR'].dropna().str.replace('-', ''))
+    cpr_onlyhk = cpr_hk.difference(cpr_db)
+    cpr_onlydb = cpr_db.difference(cpr_hk)
+    print('The following cpr are only in NovoZymes database:\n' + '\n'.join(cpr_onlydb))
+    print('The following cpr are only in HK database:\n' + '\n'.join(cpr_onlyhk))
+
+
+def compare_maillist(database, medlem_bool, maillist):
+    """ Compare main database to email list """
+    mails_db = set(database['Mailadresse'][medlem_bool].dropna().str.lower())
+    mails_txt = set(maillist.dropna().str.lower())
+    mails_del = mails_txt.difference(mails_db)
+    mails_add = mails_db.difference(mails_txt)
+    print('The following mails can be deleted from outlook:\n' + '\n'.join(mails_del))
+    print('The following mails can be added to outlook:\n' + '\n'.join(mails_add))
--- DatabaseComparer.py
+++ DatabaseComparer.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PyQt5 import uic, QtCore
+import CompareFiles
 import os
 
 qtCreatorFile = 'DatabaseComparerGui.ui'  # Enter file here.
@@ -12,6 +13,9 @@ class EmittingStream(QtCore.QObject):
 
     def write(self, text):
         self.textWritten.emit(str(text))
+
+    def flush(self):
+        pass
 
 
 class MyApp(QMainWindow):
@@ -27,6 +31,11 @@ class MyApp(QMainWindow):
         self.hk_file_path = ''
         self.emails_file_path = ''
         self.last_dir = ''
+        self.database = None
+        self.medlem = None
+        self.vilkar = None
+        self.hk = None
+        self.maillist = None
 
         # Install the custom output stream
         sys.stdout = EmittingStream(textWritten=self.normalOutputWritten)
@@ -52,37 +61,43 @@ class MyApp(QMainWindow):
         self.db_file_path, _ = QFileDialog.getOpenFileName(self, "Load database file", self.last_dir, "Excel Files (*.xlsx)")
         self.last_dir = os.path.dirname(self.db_file_path)
         self.ui.textDb.setText(self.db_file_path)
-        print('Database loaded from: ' + self.db_file_path)
+        print('Database set to: ' + self.db_file_path)
 
     def select_vilkar(self):
         self.vilkar_file_path, _ = QFileDialog.getOpenFileName(self, "Load vilkar file", self.last_dir, "Excel Files (*.xlsx)")
         self.last_dir = os.path.dirname(self.vilkar_file_path)
         self.ui.textVilkar.setText(self.vilkar_file_path)
-        print('Vilkar loaded from: ' + self.vilkar_file_path)
+        print('Vilkar set to: ' + self.vilkar_file_path)
 
     def select_hk(self):
         self.hk_file_path, _ = QFileDialog.getOpenFileName(self, "Load hk file", self.last_dir, "Excel Files (*.xlsx)")
         self.last_dir = os.path.dirname(self.hk_file_path)
         self.ui.textHk.setText(self.hk_file_path)
-        print('HK loaded from: ' + self.hk_file_path)
+        print('HK set to: ' + self.hk_file_path)
 
     def select_emails(self):
-        self.txt_file_path, _ = QFileDialog.getOpenFileName(self, "Load email file", self.last_dir, "Text Files (*.txt)")
-        self.last_dir = os.path.dirname(self.txt_file_path)
-        self.ui.textEmails.setText(self.txt_file_path)
-        print('Emails loaded from: ' + self.txt_file_path)
+        self.emails_file_path, _ = QFileDialog.getOpenFileName(self, "Load email file", self.last_dir, "Text Files (*.txt)")
+        self.last_dir = os.path.dirname(self.emails_file_path)
+        self.ui.textEmails.setText(self.emails_file_path)
+        print('Emails set to: ' + self.emails_file_path)
 
     def compare_init(self):
-        print('compare init')
+        self.database, self.medlem = CompareFiles.read_db(self.db_file_path)
+        self.vilkar = CompareFiles.read_vilkar(self.vilkar_file_path)
+        CompareFiles.compare_vilkar(self.database, self.vilkar)
 
     def compare_cpr(self):
-        print('compare cpr')
+        self.database, self.medlem = CompareFiles.read_db(self.db_file_path)
+        self.hk = CompareFiles.read_hk(self.hk_file_path)
+        CompareFiles.compare_hk(self.database, self.hk)
 
     def compare_emails(self):
-        print('compare emails')
+        self.database, self.medlem = CompareFiles.read_db(self.db_file_path)
+        self.maillist = CompareFiles.read_maillist(self.emails_file_path)
+        CompareFiles.compare_maillist(self.database, self.medlem, self.maillist)
 
     def normalOutputWritten(self, text):
-        """Append text to the QTextEdit."""
+        """ Append text to the QTextEdit """
         self.ui.textConsole.append(text)
 
 if __name__ == "__main__":
